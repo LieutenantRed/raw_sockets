493634d8e1452f4df8871c16ca2372338c68a248
marwolotmm <SallTWay@gmail.com>
2019-03-22 20:45:02 +0700

    ip header added

--- raw_client/src/main.c
+++ raw_client/src/main.c
@@ -8,7 +8,8 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
-#include <linux/udp.h>
+
+#define PROTO_UDP 17
 
 #define BUFFER_SIZE 1024
 #define HOST "192.168.1.7"
@@ -23,52 +24,90 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 
-	struct sockaddr_in host_addr;
-	memset(&host_addr, 0, sizeof(host_addr));
+	int ip_opt_enabled = 1;
+	setsockopt(raw_udp, IPPROTO_IP, IP_HDRINCL, &ip_opt_enabled, sizeof(int));
 	
-	host_addr.sin_family = AF_INET;
-	host_addr.sin_port = htons(HOST_PORT);
-	inet_aton(HOST, &host_addr.sin_addr);
+	struct {
+		unsigned char ihl:4, ver:4; 
+		unsigned char tos;
+		uint16_t len;
+		uint16_t id;
+		uint16_t flg_off;
+		unsigned char ttl;
+		unsigned char proto;
+		uint16_t csum;
+		uint32_t ip_src;
+		uint32_t ip_dst;
+	} ip_head;
 
 	struct {
 		uint16_t src;
 		uint16_t dst;
 		uint16_t len;
 		uint16_t csum;
-	} head;
-	memset(&head, 0, sizeof(head));
+	} udp_head;
 
-	char buffer[BUFFER_SIZE];
-	memset(buffer, 0, BUFFER_SIZE);
+	/*IP HEADER*/
+	memset(&ip_head, 0, sizeof(ip_head));
 
-	head.src = htons(THIS_PORT);
-	head.dst = htons(HOST_PORT);
-	head.len = htons(BUFFER_SIZE);
-	head.csum = 0;
+	ip_head.ihl = 5;
+	ip_head.ver = 4;
+	ip_head.len = htons(sizeof(ip_head) + sizeof(udp_head));
+	ip_head.ttl = 255;
+	ip_head.proto = PROTO_UDP;
+	inet_aton(THIS, (struct in_addr*)&(ip_head.ip_src)); 
+	inet_aton(HOST, (struct in_addr*)&(ip_head.ip_dst));
 
-	memcpy(&buffer, &head, sizeof(head));
+	/*UDP HEADER*/
+	memset(&udp_head, 0, sizeof(udp_head));
 
-	
+	udp_head.src = htons(THIS_PORT);
+	udp_head.dst = htons(HOST_PORT);
+	udp_head.len = htons(BUFFER_SIZE);
+	udp_head.csum = 0;
+
+
+	/*MSG BUFFER*/
+	char buffer[BUFFER_SIZE + sizeof(udp_head) + sizeof(ip_head)];
+	memset(buffer, 0, BUFFER_SIZE + sizeof(udp_head) + sizeof(ip_head));
+
+	memcpy(buffer, &ip_head, sizeof(ip_head));
+	memcpy((char*)buffer + sizeof(ip_head), &udp_head, sizeof(udp_head));
+
+	struct sockaddr_in host_addr;
+	memset(&host_addr, 0, sizeof(host_addr));
+	host_addr.sin_family = AF_INET;
+	host_addr.sin_port = htons(HOST_PORT);
+	inet_aton(HOST, (struct in_addr*)&(host_addr.sin_addr));
+
+	/*PUSH MSG*/
 	while (1) {
 		//clear
-		memset(buffer + sizeof(head), 0, BUFFER_SIZE - sizeof(head));
+		memset(buffer + sizeof(udp_head) + sizeof(ip_head), 0, BUFFER_SIZE);
 
 		//send
-		fgets(buffer + sizeof(head), BUFFER_SIZE - sizeof(head), stdin);
+		fgets(buffer + sizeof(udp_head) + sizeof(ip_head), BUFFER_SIZE, stdin);
 
-		int len = sizeof(host_addr);
-		sendto(raw_udp, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&host_addr, len);
-		if (strcmp(buffer + sizeof(head), "exit\n") == 0) {
+		sendto(
+			raw_udp, 
+			buffer, 
+			BUFFER_SIZE + sizeof(udp_head) + sizeof(ip_head), 
+			0, 
+			(struct socksaddr*)&host_addr, 
+			sizeof(host_addr)
+		);
+
+		if (strcmp(buffer + sizeof(udp_head) + sizeof(ip_head), "exit\n") == 0) {
 			break;
 		}
 
 		//recv
 		char printbuf[BUFFER_SIZE];
 		memset(printbuf, 0, BUFFER_SIZE);
-		memset(&head, 0, sizeof(head));
-		while (ntohs(head.dst) != THIS_PORT) {
+		memset(&udp_head, 0, sizeof(udp_head));
+		while (ntohs(udp_head.dst) != THIS_PORT) {
 			recvfrom(raw_udp, printbuf, BUFFER_SIZE, 0, 0, 0);
-			memcpy(&head, printbuf + 20, sizeof(head));
+			memcpy(&udp_head, printbuf + 20, sizeof(udp_head));
 		}
 		printf("%s", printbuf + 20 + 8);
 	}
